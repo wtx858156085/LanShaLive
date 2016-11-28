4c1c3dee3a8ef941613b75783c45c1abf0e81096
Wind <858156085@qq.com>
2016-11-28 10:59:23 +0800

    inti1

--- app/src/main/java/com/lansha/lanshalive/MainActivity.java
+++ app/src/main/java/com/lansha/lanshalive/MainActivity.java
@@ -17,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
         Log.e("", "init: " );
         Log.e("", "init: " );
         Log.e("", "init: " );
+        Log.e("", "init: " );
     }
 }
